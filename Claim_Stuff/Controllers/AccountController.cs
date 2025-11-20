0ac776b2300ebcbbf9d77ec4271ccb5ec50704d0
Mentor Mashaun <012mentor@gmail.com>
2025-11-20 23:01:53 +0200

    commit

--- Claim_Stuff/Controllers/AccountController.cs
+++ Claim_Stuff/Controllers/AccountController.cs
@@ -1,81 +1,81 @@
 ﻿using Claim_Stuff.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Win32;
 
 namespace Claim_Stuff.Controllers
 {
     public class AccountController : Controller
     {
+        // ---------------- REGISTER ----------------
+
         [HttpGet]
         public IActionResult Register()
         {
-            //creating an instance for the class sql_query
+            // Create table on first load
             sql_queries_register queries = new sql_queries_register();
-            //call the method create_table to create the table
             queries.create_table();
+
             return View();
         }
 
         [HttpPost]
         public IActionResult Register(Register model)
         {
-            if (ModelState.IsValid)
-            {
-                sql_queries_register get_values = new sql_queries_register();
-                get_values.store_user(model.name, model.email, model.username, model.password, model.role);
-                TempData["SuccessMessage"] = "Registration successful!";
-                return RedirectToAction("Login");
-            }
-            return View(model);
+            if (!ModelState.IsValid)
+                return View(model);
+
+            sql_queries_register db = new sql_queries_register();
+            db.store_user(model.name, model.email, model.username, model.password, model.role);
+
+            TempData["SuccessMessage"] = "Registration successful!";
+            return RedirectToAction("Login");
         }
-        //Get
+
+        // ---------------- LOGIN ----------------
+
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
-        // POST: /Account/Login
         [HttpPost]
         public IActionResult Login(string username, string password, string role)
         {
-            sql_queries_register search_values = new sql_queries_register();
-            var user = search_values.Login_user(username, password, role);
+            sql_queries_register db = new sql_queries_register();
+            bool isValidUser = db.Login_user(username, password, role);
 
-            if (user)
+            if (!isValidUser)
             {
-                ViewBag.Username = username;
-                ViewBag.Role = role;
-                HttpContext.Session.SetString("role", role);
-                if (role == "Lecturer")
-                    return RedirectToAction("Lecturer");
-                else if (role == "PC")
-                    return RedirectToAction("ProgramCoordinator");
-                else if (role == "PM")
-                    return RedirectToAction("ProgramManager");
-                else if (role == "HR")
-                    return RedirectToAction("HR");
-                else
-                    return RedirectToAction("Login");
+                ViewBag.Error = "Invalid username, password, or role.";
+                return View();
             }
 
-            ViewBag.Error = "Invalid username or password.";
-            return View();
+            // Store session info
+            HttpContext.Session.SetString("Role", role);
+
+            // Redirect based on role
+            return role switch
+            {
+                "Lecturer" => RedirectToAction("Lecturer"),
+                "PC" => RedirectToAction("ProgramCoordinator"),
+                "PM" => RedirectToAction("ProgramManager"),
+                "HR" => RedirectToAction("HR"),
+                _ => RedirectToAction("Login")
+            };
         }
 
-        // Lecturer Dashboard
+        // ---------------- DASHBOARDS ----------------
+
         public IActionResult Lecturer()
         {
             return View();
         }
 
-        // Program Coordinator Dashboard
         public IActionResult ProgramCoordinator()
         {
             return View();
         }
 
-        // Program Manager Dashboard
         public IActionResult ProgramManager()
         {
             return View();
@@ -85,12 +85,13 @@ namespace Claim_Stuff.Controllers
         {
             return View();
         }
-        // Logout
+
+        // ---------------- LOGOUT ----------------
+
         public IActionResult Logout()
         {
-            return View();
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
         }
-
-
     }
 }
